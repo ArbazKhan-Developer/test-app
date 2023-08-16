5a3c6050a39c68a85fd83c17f03e1f8beaca93a8
ArbazKhan-Developer <kkabz860156@gmail.com>
2023-08-16 16:27:08 +0530

    code updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express()
-const PORT = 3000
+const PORT = 80
 
 app.get('/', (req,res)=>{
-    res.send('Hello there, welcome to the EC2 instance')
+    res.send('Hello there, welcome to the EC2 instance running on port 80')
 })
 
 app.get('/home',(req,res)=>{
